20198320c7432776314a5909b25d1a319d79d7b9
Sofia Murillo <sof2114987@maricopa.edu>
2025-11-08 16:56:25 -0700

    First commit

---
+++ f25_murillo.py
@@ -0,0 +1,5 @@
+print("""\n\"We tell ourselves stories in order to live. We live entirely by the impression 
+of a narrative line upon disparate images, the shifting phantasmagoria, 
+which is our actual experience.\"
+
+- Joan Didion (The White Album, 1979)""")
